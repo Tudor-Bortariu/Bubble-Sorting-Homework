a46af3979fd16b87d07c41b7ec4a7c7a092a604f
Bortariu Tudor <106099630+Tudor-Bortariu@users.noreply.github.com>
2022-06-11 16:43:53 +0300

    Added javadoc comments.

--- src/main/java/ro/siit/bubbleSortingHomework/BubbleSortMethod.java
+++ src/main/java/ro/siit/bubbleSortingHomework/BubbleSortMethod.java
@@ -1,6 +1,14 @@
 package ro.siit.bubbleSortingHomework;
 
 public class BubbleSortMethod {
+    /**
+     * This is the bubble sort function. At the beginning of the while, it assigns a boolean variable to false in order for the
+     * if function to start and proceed with the loop. The loop compares the number at position i with the number at position i+1
+     * and it swaps them if the condition is met. The loop will stop only when it has nothing to swap. The swap is done using the
+     * temporary variable.
+     * @param numbers represents an array of objects from the SalesRepresentatives class.
+     */
+
     public static void sortSalesValues(SalesRepresentatives[] numbers) {
         boolean swap = true;
 
--- src/main/java/ro/siit/bubbleSortingHomework/Main.java
+++ src/main/java/ro/siit/bubbleSortingHomework/Main.java
@@ -1,8 +1,12 @@
 package ro.siit.bubbleSortingHomework;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        /**
+         * This code creates an array of 5 objects.
+         */
         SalesRepresentatives [] salesReport = new SalesRepresentatives[5];
         salesReport[0] = new SalesRepresentatives("John Doe", 22, 450.0);
         salesReport[1] = new SalesRepresentatives("Jane Doe", 15, 722);
@@ -10,13 +14,22 @@ public class Main {
         salesReport[3] = new SalesRepresentatives("Max Stones", 12, 583);
         salesReport[4] = new SalesRepresentatives("Sarah James", 30, 498);
 
+        /**
+         * This prints out the objects in the array before the sorting method is applied.
+         */
         System.out.println("Before:");
         for (SalesRepresentatives representatives : salesReport) {
             System.out.println(representatives);
         }
 
+        /**
+         * This code applies the sorting method from the BubbleSortMethod class to the array of objects.
+         */
         BubbleSortMethod.sortSalesValues(salesReport);
 
+        /**
+         * This loop prints the objects in the array after the sorting method is applied.
+         */
         System.out.println("After:");
         for (SalesRepresentatives salesRepresentatives : salesReport) {
             System.out.println(salesRepresentatives);
